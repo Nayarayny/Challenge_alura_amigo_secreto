61ab1b40f7395b03e336037c5837351080a92d04
Nayara Ribeiro <nayaraallisonnay@gmail.com>
2025-01-30 10:28:50 -0300

    Colocando nomes no array

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value;
+
+    if (nome === "") {
+        alert ('Por Favor insira um nome!');
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+        nome = document.querySelector('input');
+        nome.value = "";
+    }
+}
+
+function listaDeAmigos(tag, nomes) {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i >= amigos.length; i++){
+        lista.innerHTML = amigos;        
+    }
+}
